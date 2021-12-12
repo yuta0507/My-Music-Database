8cbf5127eb7e8edcad6c2361943c479d022ef160
yuta0507 <yutakikkawa0507@gmail.com>
2021-12-13 00:30:20 +0900

    First Commit

---
+++ src/resources/views/sample.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>Title</title>
+</head>
+<body>
+<h1>sample</h1>
+<div id="app">
+    <example-component></example-component>
+</div>
+
+<script src="js/app.js"></script>
+</body>
+</html>
